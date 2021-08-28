e13057c40f50f8f6a6a84042b55b159d707d08a6
Melvin Biamont <melvin.biamont@gmail.com>
2021-08-28 17:55:48 -0300

    fix(): Fix Sonatype credentials

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,8 +116,8 @@ tasks {
             sonatype {
                 nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
                 snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-                username.set(System.getenv("SONATYPE_USERNAME"))
-                password.set(System.getenv("SONATYPE_PASSWORD"))
+                username.set(project.property("SONATYPE_USERNAME") as String?)
+                password.set(project.property("SONATYPE_PASSWORD") as String?)
             }
         }
     }
