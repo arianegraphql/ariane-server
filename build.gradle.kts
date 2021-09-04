2c6b3d5ccbe1fb54927330aa2aade00dbd03b400
Melvin Biamont <melvin.biamont@gmail.com>
2021-09-04 19:42:50 -0300

    impr(): Move to Gradle 7.2 and Java 8 (#5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.arianegraphql"
-version = "0.0.3"
+version = "0.0.4"
 
 repositories {
     mavenCentral()
--- server-ktor/build.gradle.kts
+++ server-ktor/build.gradle.kts
@@ -1,11 +1,11 @@
 group = project.rootProject.group
 version = project.rootProject.version
 
-val ktorVersion = "1.6.2"
+val ktorVersion = "1.6.3"
 
 dependencies {
     implementation(project(":server"))
     implementation("io.ktor:ktor-server-cio:$ktorVersion")
     implementation("io.ktor:ktor-websockets:$ktorVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.4")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
 }
