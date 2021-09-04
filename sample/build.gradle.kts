91d189afb0210b4b3d481463a5cdc1f2febc8cbb
Melvin Biamont <melvin.biamont@gmail.com>
2021-09-04 19:39:00 -0300

    impr(): Move to Gradle 7.2 and Java 8

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -11,9 +11,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
 
     implementation(project(":server"))
     implementation(project(":server-ktor"))
-    implementation("io.ktor:ktor-server-cio:1.6.0")
+    implementation("io.ktor:ktor-server-cio:1.6.3")
 }
